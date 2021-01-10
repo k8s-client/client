26cbd0cc0aa24f7e0a1e209c22a67b8ed5ecdd30
Chad Sikorra <chad.sikorra@gmail.com>
2021-01-10 17:48:08 -0600

    Add some metadata tests.

---
+++ tests/unit/K8s/Metadata/KindMetadataTest.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * This file is part of the k8s/client library.
+ *
+ * (c) Chad Sikorra <Chad.Sikorra@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace unit\K8s\Metadata;
+
+use K8s\Client\Metadata\KindMetadata;
+use K8s\Core\Annotation\Kind;
+use unit\K8s\Client\TestCase;
+
+class KindMetadataTest extends TestCase
+{
+    /**
+     * @var Kind
+     */
+    private $kind;
+
+    /**
+     * @var KindMetadata
+     */
+    private $subject;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->kind = new Kind();
+        $this->kind->kind = 'Pod';
+        $this->kind->version = 'v1';
+        $this->subject = new KindMetadata($this->kind);
+    }
+
+    public function testGetKind(): void
+    {
+        $this->assertEquals('Pod', $this->subject->getKind());
+    }
+
+    public function testGetVersion(): void
+    {
+        $this->assertEquals('v1', $this->subject->getVersion());
+    }
+
+    public function testGetGroupWhenNotSet(): void
+    {
+        $this->assertNull($this->subject->getGroup());
+    }
+
+    public function testGetGroup(): void
+    {
+        $this->kind->group = 'app';
+
+        $this->assertEquals('app', $this->subject->getGroup());
+    }
+}
---
+++ tests/unit/K8s/Metadata/ModelMetadataTest.php
@@ -0,0 +1,96 @@
+<?php
+
+/**
+ * This file is part of the k8s/client library.
+ *
+ * (c) Chad Sikorra <Chad.Sikorra@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace unit\K8s\Metadata;
+
+use K8s\Api\Model\Api\Core\v1\Pod;
+use K8s\Client\Exception\RuntimeException;
+use K8s\Client\Metadata\KindMetadata;
+use K8s\Client\Metadata\ModelMetadata;
+use K8s\Client\Metadata\ModelPropertyMetadata;
+use K8s\Client\Metadata\OperationMetadata;
+use unit\K8s\Client\TestCase;
+
+class ModelMetadataTest extends TestCase
+{
+    /**
+     * @var array
+     */
+    private $properties;
+
+    /**
+     * @var array
+     */
+    private $operations;
+
+    /**
+     * @var KindMetadata|\Mockery\LegacyMockInterface|\Mockery\MockInterface
+     */
+    private $kind;
+
+    /**
+     * @var ModelMetadata
+     */
+    private $subject;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->properties = [\Mockery::spy(ModelPropertyMetadata::class)];
+        $this->operations = [\Mockery::spy(OperationMetadata::class)];
+        $this->kind = \Mockery::spy(KindMetadata::class);
+        $this->subject = new ModelMetadata(
+            Pod::class,
+            $this->properties,
+            $this->operations,
+            $this->kind
+        );
+    }
+
+    public function testGetModelFqcn(): void
+    {
+        $this->assertEquals(Pod::class, $this->subject->getModelFqcn());
+    }
+
+    public function testGetOperations(): void
+    {
+        $this->assertEquals($this->operations, $this->subject->getOperations());
+    }
+
+    public function testGetProperties(): void
+    {
+        $this->assertEquals($this->properties, $this->subject->getProperties());
+    }
+
+    public function testGetKind(): void
+    {
+        $this->assertEquals($this->kind, $this->subject->getKind());
+    }
+
+    public function testGetOperationByType(): void
+    {
+        $this->operations[0]->shouldReceive('getType')->andReturn('list');
+
+        $result = $this->subject->getOperationByType('list');
+        $this->assertEquals($this->operations, [$result]);
+    }
+
+    public function testGetOperationByTypeThrowsExceptionWhenNotFound(): void
+    {
+        $this->operations[0]->shouldReceive('getType')->andReturn('foo');
+
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessageMatches('/^The operation type "list" is not recognised/');
+        $this->subject->getOperationByType('list');
+    }
+}
