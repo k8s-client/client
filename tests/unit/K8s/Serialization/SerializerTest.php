a029e2e7f9e3b84d4c7ad52fdc2d44f52b0b857d
Chad Sikorra <chad.sikorra@gmail.com>
2021-01-10 18:51:25 -0600

    Add a Serializer test.

---
+++ tests/unit/K8s/Serialization/SerializerTest.php
@@ -0,0 +1,85 @@
+<?php
+
+/**
+ * This file is part of the k8s/client library.
+ *
+ * (c) Chad Sikorra <Chad.Sikorra@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace unit\K8s\Serialization;
+
+use K8s\Api\Model\Api\Core\v1\Pod;
+use K8s\Client\Metadata\MetadataCache;
+use K8s\Client\Serialization\ModelDenormalizer;
+use K8s\Client\Serialization\ModelNormalizer;
+use K8s\Client\Serialization\Serializer;
+use unit\K8s\Client\TestCase;
+
+class SerializerTest extends TestCase
+{
+    /**
+     * @var MetadataCache|\Mockery\LegacyMockInterface|\Mockery\MockInterface
+     */
+    private $cache;
+
+    /**
+     * @var ModelNormalizer|\Mockery\LegacyMockInterface|\Mockery\MockInterface
+     */
+    private $normalizer;
+
+    /**
+     * @var ModelDenormalizer|\Mockery\LegacyMockInterface|\Mockery\MockInterface
+     */
+    private $denormalizer;
+
+    /**
+     * @var Serializer
+     */
+    private $subject;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->cache = \Mockery::spy(MetadataCache::class);
+        $this->normalizer = \Mockery::spy(ModelNormalizer::class);
+        $this->denormalizer = \Mockery::spy(ModelDenormalizer::class);
+        $this->subject = new Serializer(
+            $this->cache,
+            $this->normalizer,
+            $this->denormalizer
+        );
+    }
+
+    public function testSerializeWithArray(): void
+    {
+        $result = $this->subject->serialize(['foo' => 'bar']);
+
+        $this->assertEquals('{"foo":"bar"}', $result);
+        $this->normalizer->shouldNotHaveReceived('normalize');
+    }
+
+    public function testSerializeWithObject(): void
+    {
+        $this->normalizer->shouldReceive('normalize')
+            ->andReturn(['pod' => 'data']);
+        $result = $this->subject->serialize(new Pod(null, []));
+
+        $this->assertEquals('{"pod":"data"}', $result);
+    }
+
+    public function testDeserialize(): void
+    {
+        $pod = new Pod(null, []);
+        $this->denormalizer->shouldReceive('denormalize')
+            ->with(['pod' => 'data'], Pod::class, $this->cache)
+            ->andReturn($pod);
+
+        $result = $this->subject->deserialize('{"pod":"data"}', Pod::class);
+        $this->assertEquals($pod, $result);
+    }
+}
