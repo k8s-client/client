a8ea522599fdd79e9f170e74605ce14e9e4f6752
Chad Sikorra <chad.sikorra@gmail.com>
2021-01-12 19:49:10 -0600

    Add an ExecConnection test.

---
+++ tests/unit/K8s/Client/Websocket/ExecConnectionTest.php
@@ -0,0 +1,70 @@
+<?php
+
+/**
+ * This file is part of the k8s/client library.
+ *
+ * (c) Chad Sikorra <Chad.Sikorra@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace unit\K8s\Client\Websocket;
+
+use K8s\Client\Websocket\ExecConnection;
+use K8s\Core\Websocket\Contract\WebsocketConnectionInterface;
+use unit\K8s\Client\TestCase;
+
+class ExecConnectionTest extends TestCase
+{
+    /**
+     * @var WebsocketConnectionInterface|\Mockery\LegacyMockInterface|\Mockery\MockInterface
+     */
+    private $connection;
+
+    /**
+     * @var ExecConnection
+     */
+    private $subject;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->connection = \Mockery::spy(WebsocketConnectionInterface::class);
+        $this->subject = new ExecConnection($this->connection);
+    }
+
+    public function testWriteln(): void
+    {
+        $this->connection->shouldReceive('send')
+            ->with("\x00foo");
+
+        $this->subject->writeln('foo');
+    }
+
+    public function testMultiWriteln(): void
+    {
+        $this->connection->shouldReceive('send')
+            ->with("\x00foo");
+        $this->connection->shouldReceive('send')
+            ->with("\x00bar");
+
+        $this->subject->writeln(['foo', 'bar']);
+    }
+
+    public function testKeepAlive(): void
+    {
+        $this->connection->shouldReceive('send')
+            ->with("\x00");
+
+        $this->subject->keepalive();
+    }
+
+    public function testClose(): void
+    {
+        $this->subject->close();
+        $this->connection->shouldHaveReceived('close');
+    }
+}
