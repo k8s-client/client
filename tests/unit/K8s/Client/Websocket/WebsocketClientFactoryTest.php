1ed23bb1ebec326f7cfb6b0a5ac8f5788f199a91
Chad Sikorra <chad.sikorra@gmail.com>
2021-01-13 19:13:07 -0600

    Add a test.

---
+++ tests/unit/K8s/Client/Websocket/WebsocketClientFactoryTest.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * This file is part of the k8s/client library.
+ *
+ * (c) Chad Sikorra <Chad.Sikorra@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace unit\K8s\Client\Websocket;
+
+use K8s\Client\Http\RequestFactory;
+use K8s\Client\Websocket\WebsocketClient;
+use K8s\Client\Websocket\WebsocketClientFactory;
+use unit\K8s\Client\TestCase;
+
+class WebsocketClientFactoryTest extends TestCase
+{
+    /**
+     * @var WebsocketClientFactory
+     */
+    private $subject;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->subject = new WebsocketClientFactory(null, \Mockery::spy(RequestFactory::class));
+    }
+
+    public function testMakeClient(): void
+    {
+        $result = $this->subject->makeClient();
+
+        $this->assertInstanceOf(WebsocketClient::class, $result);
+    }
+}
