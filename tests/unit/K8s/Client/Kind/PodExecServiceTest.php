847e1b9f9c195e69a03b6b0ecb2c6df3ab03a3af
Chad Sikorra <chad.sikorra@gmail.com>
2021-01-14 19:10:59 -0600

    Fix the class namespace.

--- tests/unit/K8s/Client/Kind/PodExecServiceTest.php
+++ tests/unit/K8s/Client/Kind/PodExecServiceTest.php
@@ -15,7 +15,7 @@ namespace unit\K8s\Client\Kind;
 
 use K8s\Api\Service\Core\v1\PodExecOptionsService;
 use K8s\Client\Kind\PodExecService;
-use K8s\Core\Websocket\Contract\ContainerExecInterface;
+use K8s\Client\Websocket\Contract\ContainerExecInterface;
 use unit\K8s\Client\TestCase;
 
 class PodExecServiceTest extends TestCase
